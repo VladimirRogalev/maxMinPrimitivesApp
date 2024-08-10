55b7ead6c431bb0573bcd6f7a73be7a95ebb7eda
Vladimir <vladimirrogalev1@gmail.com>
2024-08-10 15:32:41 +0300

    Finished app

--- src/_01_HW_MaxMinPrimitivesApp/maxMinPrimitivesApp.java
+++ src/_01_HW_MaxMinPrimitivesApp/maxMinPrimitivesApp.java
@@ -3,8 +3,45 @@ package _01_HW_MaxMinPrimitivesApp;
 public class maxMinPrimitivesApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		result(args[0].toLowerCase());
+
+	}
+
+	public static void result(String oper) {
+		switch (oper) {
+		case "byte":
+			System.out.println("Byte min: " + Byte.MIN_VALUE);
+			System.out.println("Byte max " + Byte.MAX_VALUE);
+			break;
+		case "integer":
+			System.out.println("Integer min: " + Integer.MIN_VALUE);
+			System.out.println("Integer max: " + Integer.MAX_VALUE);
+			break;
+		case "short":
+			System.out.println("Short min: " + Short.MIN_VALUE);
+			System.out.println("Short max: " + Short.MAX_VALUE);
+			break;
+		case "long":
+			System.out.println("Long min: " + Long.MIN_VALUE);
+			System.out.println("Long max: " + Long.MAX_VALUE);
+			break;
+		case "char":
+			System.out.println("Char min: " + (int)Character.MIN_VALUE);
+			System.out.println("Char max: " + (int)Character.MAX_VALUE);
+			break;
+		case "float":
+			System.out.println("Float min: " + Float.MIN_VALUE);
+			System.out.println("Float Max: " + Float.MAX_VALUE);
+			break;
+		case "double":
+			System.out.println("Double min: " + Double.MIN_VALUE);
+			System.out.println("Double max: " + Double.MAX_VALUE);
+			break;
+		default:
+			System.out.println("Wrong operation");
+
+		}
 	}
 
 }
